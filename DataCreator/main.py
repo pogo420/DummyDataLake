b896a68bb633d8cae043234db76fe7e738e7c48f
pogo420 <am0072008@gmail.com>
2021-03-23 17:52:51 +0530

    data creator pubsub push

--- DataCreator/main.py
+++ DataCreator/main.py
@@ -1,9 +1,13 @@
 import uuid
 import random
 import string
+from google.cloud import pubsub_v1
+import json
+import os
 
 
-def generate_random_data():
+def generate_random_data() -> dict:
+    """Function to generate random data"""
     data = {
         "userId": uuid.uuid4().__str__(),
         "sensorValue": random.random(),
@@ -15,4 +19,23 @@ def generate_random_data():
     }
     return data
 
-print(generate_random_data())
+
+def pushing_message(project_id: str, topic_id: str, message: str):
+    """Function to push message into pubsub topic"""
+    publisher = pubsub_v1.PublisherClient()
+    # The `topic_path` method creates a fully qualified identifier
+    # in the form `projects/{project_id}/topics/{topic_id}`
+    topic_path = publisher.topic_path(project_id, topic_id)
+
+    # Data must be a bytestring
+    message = message.encode("utf-8")
+    # When you publish a message, the client returns a future.
+    publisher.publish(topic_path, message)
+    print(f"Published messages to {topic_path}.")
+
+
+def main():
+    message = json.dumps(generate_random_data())
+    project_id = os.environ["GOOGLE_PROJECT"]
+    input_topic = os.environ["INPUT_TOPIC"]
+    pushing_message(project_id, input_topic, message)
