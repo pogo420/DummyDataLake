17a2454fe75368da2631210a34f069c18b2693fb
pogo420 <am0072008@gmail.com>
2021-03-28 20:30:51 +0530

    Universl sink

--- DataFlows/src/main/java/templates/PubSubToAny.java
+++ DataFlows/src/main/java/templates/PubSubToAny.java
@@ -1,46 +1,13 @@
 package templates;
 
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
-import org.apache.beam.sdk.options.Description;
-import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.options.ValueProvider;
-import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.transforms.windowing.FixedWindows;
-import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.values.PCollection;
-import org.apache.beam.sdk.values.TypeDescriptors;
-import org.joda.time.Duration;
+import transforms.UniversalSink;
 
 public class PubSubToAny {
-
-    public interface Options extends PipelineOptions{
-        // interface for defining options
-
-        @Description("input subscription")
-        ValueProvider<String> getInputSubscription();
-        void setInputSubscription(ValueProvider<String> value);
-
-        @Description("output file path")
-        ValueProvider<String> getOutputFile();
-        void setOutputFile(ValueProvider<String> value);
-
-        @Description("output topic")
-        ValueProvider<String> getOutputTopic();
-        void setOutputTopic(ValueProvider<String> value);
-
-        @Description("output BQ table")
-        ValueProvider<String> getOutputBqTable();
-        void setOutputBqTable(ValueProvider<String> value);
-
-        @Description("output BT table")
-        ValueProvider<String> getOutputBtTable();
-        void setOutputBtTable(ValueProvider<String> value);
-
-    }
 
     public static void main(String[] args) {
         Options options = PipelineOptionsFactory
@@ -60,21 +27,22 @@ public class PubSubToAny {
                         .readMessagesWithAttributes()
                         .fromSubscription(options.getInputSubscription()));
 
+        pubSubData.apply("Sink processing", UniversalSink.build(options));
             // Writing into text file
-            pubSubData
-                    .apply("Converting PubSub messages to String", MapElements
-                    .into(TypeDescriptors.strings())
-                    .via(PubsubMessage::toString))
-
-                    .apply("Windowing", Window
-                            .into(FixedWindows.of(Duration.standardMinutes(2))))
-
-                    .apply("Writing to file", TextIO
-                            .write()
-                            .to(options.getOutputFile())
-                            .withWindowedWrites()
-                            .withNumShards(1)
-                    );
+//            pubSubData
+//                    .apply("Converting PubSub messages to String", MapElements
+//                    .into(TypeDescriptors.strings())
+//                    .via(PubsubMessage::toString))
+//
+//                    .apply("Windowing", Window
+//                            .into(FixedWindows.of(Duration.standardMinutes(2))))
+//
+//                    .apply("Writing to file", TextIO
+//                            .write()
+//                            .to(options.getOutputFile())
+//                            .withWindowedWrites()
+//                            .withNumShards(1)
+//                    );
         pipeline.run();
     }
 }
