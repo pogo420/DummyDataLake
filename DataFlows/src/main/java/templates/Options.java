7b35dcca25e8995a8f161598a03aada6ece68aed
pogo420 <am0072008@gmail.com>
2021-04-15 20:17:21 +0530

    Datflow options update

--- DataFlows/src/main/java/templates/Options.java
+++ DataFlows/src/main/java/templates/Options.java
@@ -10,6 +10,14 @@ public interface Options extends PipelineOptions {
     String getOutputFile();
     void setOutputFile(String value);
 
+    @Description("output topic name")
+    String getOutputTopic();
+    void setOutputTopic(String value);
+
+    @Description("output bq table name")
+    String getBqTable();
+    void setBqTable(String value);
+
     @Description("input subscription")
     String getInputSubscription();
     void setInputSubscription(String value);
