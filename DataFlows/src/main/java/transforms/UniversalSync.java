3c5d4e527c5a617b0aed8fc63e7e0993b90cd8f2
pogo420 <am0072008@gmail.com>
2021-04-25 22:04:24 +0530

    Univeral sync

--- DataFlows/src/main/java/transforms/UniversalSync.java
+++ DataFlows/src/main/java/transforms/UniversalSync.java
@@ -1,6 +1,8 @@
 package transforms;
 
+import com.google.api.services.bigquery.model.TableRow;
 import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.PTransform;
@@ -27,29 +29,31 @@ public class UniversalSync extends PTransform<PCollection<PubsubMessage>, PDone>
     @Override
     public PDone expand(PCollection<PubsubMessage> input) {
 
-        PCollection<String> windowedData = input
-                .apply("Converting PubSub messages to String", MapElements
-                        .into(TypeDescriptors.strings())
-                        .via(message -> new String(message.getPayload(), StandardCharsets.UTF_8)))
-
-                .apply("Windowing", Window
-                        .into(FixedWindows.of(Duration.standardSeconds(200))));
-
         if (!this.outputFile.isEmpty()) {
-            windowedData
+
+            input
+                    .apply("Converting PubSub messages to String", MapElements
+                            .into(TypeDescriptors.strings())
+                            .via(message -> new String(message.getPayload(), StandardCharsets.UTF_8)))
+
+                    .apply("Windowing", Window
+                            .into(FixedWindows.of(Duration.standardSeconds(200))))
+
                     .apply("Writing to file", TextIO
                             .write()
                             .to(this.outputFile)
                             .withWindowedWrites()
                             .withNumShards(1));
+
         }
 
-        if (!this.outputBqTable.isEmpty()){
-            windowedData
-                    .apply("Writing to BQ table", null);
+        if (!this.outputBqTable.isEmpty()) {
+
+            input
+                    .apply("Writing to BQ", null);
+
         }
         return PDone.in(input.getPipeline());
     }
-
 
 }
