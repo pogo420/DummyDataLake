c6f904d8b7f0385482ec4f4ad6b5f5f8a3429d07
Your Name <you@example.com>
2021-05-10 11:41:19 +0530

    adding TS logic

--- DataFlows/src/main/java/messages/IngestionMessage.java
+++ DataFlows/src/main/java/messages/IngestionMessage.java
@@ -2,10 +2,10 @@ package messages;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class IngestionMessage {
+    /** Class for Managing Ingestion Message */
 
     public static final String PROCESSING_TIME = "_pt";
     public static final String PAYLOAD = "_pl";
@@ -19,19 +19,23 @@ public class IngestionMessage {
    }
 
     public ObjectNode getPayload() {
+        // return the payload
         return payload;
     }
 
     public String messageSerial() {
+        // returns serialized message
         return Json.serialize(this);
     }
 
     public static IngestionMessage messageDeSerial(String json) {
+        // returns de serialized message
         return Json.deserialize(json, IngestionMessage.class);
     }
 
     public static IngestionMessage setPayload(ObjectNode node) {
-        return new IngestionMessage((node));
+        // creating new payload
+        return new IngestionMessage(node);
     }
 
 }
--- DataFlows/src/main/java/transforms/Validations.java
+++ DataFlows/src/main/java/transforms/Validations.java
@@ -1,14 +1,14 @@
 package transforms;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import messages.IngestionMessage;
-import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
 import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionTuple;
+import org.apache.beam.sdk.values.TypeDescriptor;
 
 import static messages.IngestionMessage.PROCESSING_TIME;
 import static messages.IngestionMessage.PAYLOAD;
@@ -30,22 +30,21 @@ public class Validations extends PTransform<PCollection<IngestionMessage>, PColl
     @Override
     public PCollectionTuple expand(PCollection<IngestionMessage> input) {
 
-        input.apply("Adding Processing Time", ParDo.of(
-                new DoFn<IngestionMessage, IngestionMessage>() {
-                    @ProcessElement
-                    public void processElement(ProcessContext c){
-                        ObjectNode objectNode = c.element().getPayload();
-                        JsonNode payload = objectNode.get(PAYLOAD);
-
-
-
-                        c.output(null);
-                    }
-                }
-            )
-        );
+        input.apply("Adding Processing Time", ParDo.of(addingProcessingTime()));
         return null;
     }
 
 
+    private DoFn<IngestionMessage, IngestionMessage> addingProcessingTime() {
+        return new DoFn<IngestionMessage, IngestionMessage>() {
+            @ProcessElement
+            public void processElement(ProcessContext c){
+                ObjectNode objectNode = c.element().getPayload();
+                ObjectNode payload = (ObjectNode) objectNode.get(PAYLOAD);
+                ObjectNode payloadWithTimeStamp = payload.put(PROCESSING_TIME, c.timestamp().toString());
+                IngestionMessage ingestionMessageWithTimeStamp = IngestionMessage.setPayload(payloadWithTimeStamp);
+                c.output(ingestionMessageWithTimeStamp);
+            }
+        };
+    }
 }
